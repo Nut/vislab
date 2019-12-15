302455e97819de0154e46064f46e7386c2db40df
Yannick Stephan <yannick_bmx12@gmx.de>
2019-12-15 16:12:48 +0100

    Implemented user service

--- user-service/src/main/java/de/hska/iwi/vslab/userservice/UserController.java
+++ user-service/src/main/java/de/hska/iwi/vslab/userservice/UserController.java
@@ -2,6 +2,11 @@ package de.hska.iwi.vslab.userservice;
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import de.hska.iwi.vslab.userservice.datamodels.NewUser;
+import de.hska.iwi.vslab.userservice.datamodels.Role;
+import de.hska.iwi.vslab.userservice.datamodels.User;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,19 +17,37 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private RoleRepository roleRepository;
+
     @RequestMapping(value = "/users", method = RequestMethod.POST)
-    public ResponseEntity<User> createNewProduct(@RequestBody User user) {
+    public ResponseEntity<User> createNewProduct(@RequestBody NewUser user) {
+
+        // check if Role exists, otherwise return 404
+        // 1 -> regular User, 2-> Admin
+        Role role = roleRepository.findByLevel(user.getRolelevel())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Role level " + user.getRolelevel() + " not found!"));
+
         try {
-            User newUser = userRepository.save(user);
+            User newUser = new User(user.getUsername(), user.getFirstname(), user.getLastname(), user.getPassword(),
+                    role);
+            userRepository.save(newUser);
             return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
     }
 
-    // @RequestMapping(value = "/users/{username}", method = RequestMethod.GET)
-    // public User getProductById(@PathVariable String username) {
-    //     return userRepository.findByUsername(username)
-    //             .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found!"));
-    // }
+    @RequestMapping(value = "/users/{username}", method = RequestMethod.GET)
+    public User getProductById(@PathVariable String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found!"));
+    }
+
+    @RequestMapping(value = "/roles/{level}", method = RequestMethod.GET)
+    public Role getRoleByLevel(@PathVariable int level) {
+        return roleRepository.findByLevel(level).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Role level " + level + " not found!"));
+    }
 }
--- user-service/src/main/java/de/hska/iwi/vslab/userservice/UserRepository.java
+++ user-service/src/main/java/de/hska/iwi/vslab/userservice/UserRepository.java
@@ -4,9 +4,11 @@ import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 
+import de.hska.iwi.vslab.userservice.datamodels.User;
+
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
 public interface UserRepository extends CrudRepository<User, Long> {
-    // Optional<User> findByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
