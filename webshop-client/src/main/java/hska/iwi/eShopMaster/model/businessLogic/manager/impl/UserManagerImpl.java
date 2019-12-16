89e85a04387be3ed978c31de3de231261fabfd8e
Yannick Stephan <yannick_bmx12@gmx.de>
2019-12-16 20:36:51 +0100

    Client can now use rest api, added logging

--- webshop-client/src/main/java/hska/iwi/eShopMaster/model/businessLogic/manager/impl/UserManagerImpl.java
+++ webshop-client/src/main/java/hska/iwi/eShopMaster/model/businessLogic/manager/impl/UserManagerImpl.java
@@ -5,6 +5,15 @@ import hska.iwi.eShopMaster.model.database.dataAccessObjects.RoleDAO;
 import hska.iwi.eShopMaster.model.database.dataAccessObjects.UserDAO;
 import hska.iwi.eShopMaster.model.database.dataobjects.Role;
 import hska.iwi.eShopMaster.model.database.dataobjects.User;
+import hska.iwi.eShopMaster.model.database.models.NewUser;
+
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+
+import hska.iwi.eShopMaster.model.database.LoggingRequestInterceptor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 
@@ -12,21 +21,33 @@ import hska.iwi.eShopMaster.model.database.dataobjects.User;
  */
 
 public class UserManagerImpl implements UserManager {
+
+	private final RestTemplate restTemplate = new RestTemplate();
+
 	UserDAO helper;
-	
+
 	public UserManagerImpl() {
 		helper = new UserDAO();
 	}
 
-	
 	public void registerUser(String username, String name, String lastname, String password, Role role) {
+		// User user = new User(username, name, lastname, password, role);
+		// helper.saveObject(user);
 
-		User user = new User(username, name, lastname, password, role);
+		List<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
+		interceptors.add(new LoggingRequestInterceptor());
+		restTemplate.setInterceptors(interceptors);
 
-		helper.saveObject(user);
+		NewUser newUser = new NewUser(username, name, lastname, password, role.getLevel());
+		String WEBSHOP_URI = "http://webshop-api:8080";
+
+		try {
+			restTemplate.postForLocation(WEBSHOP_URI + "/users", newUser);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 
-	
 	public User getUserByUsername(String username) {
 		if (username == null || username.equals("")) {
 			return null;
@@ -47,23 +68,22 @@ public class UserManagerImpl implements UserManager {
 	}
 
 	public boolean doesUserAlreadyExist(String username) {
-		
-    	User dbUser = this.getUserByUsername(username);
-    	
-    	if (dbUser != null){
-    		return true;
-    	}
-    	else {
-    		return false;
-    	}
-	}
-	
 
-	public boolean validate(User user) {
-		if (user.getFirstname().isEmpty() || user.getPassword().isEmpty() || user.getRole() == null || user.getLastname() == null || user.getUsername() == null) {
+		User dbUser = this.getUserByUsername(username);
+
+		if (dbUser != null) {
+			return true;
+		} else {
 			return false;
 		}
-		
+	}
+
+	public boolean validate(User user) {
+		if (user.getFirstname().isEmpty() || user.getPassword().isEmpty() || user.getRole() == null
+				|| user.getLastname() == null || user.getUsername() == null) {
+			return false;
+		}
+
 		return true;
 	}
 
