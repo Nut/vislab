5a69a59399dc2d5e2f5050f94acb91f429764fc9
Yannick Stephan <yannick_bmx12@gmx.de>
2019-12-16 23:06:18 +0100

    Removed unused import

--- webshop-api/src/main/java/de/hska/iwi/vslab/webshopapi/WebshopApiController.java
+++ webshop-api/src/main/java/de/hska/iwi/vslab/webshopapi/WebshopApiController.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 
